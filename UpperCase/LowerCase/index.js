05e348176933130761a01db3e83a2c55a3bb895f
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-05 20:26:53 +0600

    More Update Code

---
+++ UpperCase/LowerCase/index.js
@@ -0,0 +1,9 @@
+// For UpperCase
+
+var UpperType = "at first you will know about varaiable declaration.";
+console.log(UpperType.toUpperCase())
+
+// For LowerCase
+
+var LowerType = "IF YOU DON'T KNOW ABOUT ANY RESERVED KEYWORD YOU DON'T BE A PROGRAMMER.";
+console.log(LowerType.toLowerCase())
