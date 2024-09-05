9d457be428752aaf28c3aed2556bb2432035a4b1
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-05 19:31:10 +0600

    Add new Code

--- Variables/index.js
+++ Variables/index.js
@@ -1,5 +1,8 @@
-var nameType = "Good Night";
+var nameType = "My Name is Naimul Hasan Nabil";
 console.log(nameType)
 
-var numType = 'Nabil';
+var numType = 'This is My First JS Code';
 console.log(numType)
+
+var BoolType = true;
+console.log(BoolType)
