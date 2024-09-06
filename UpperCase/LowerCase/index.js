da58bda911bc310a7737dae36854f64553b724ea
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-06 11:08:08 +0600

    Add UpperCase and LowerCase Code

--- UpperCase/LowerCase/index.js
+++ UpperCase/LowerCase/index.js
@@ -5,5 +5,5 @@ console.log(UpperType.toUpperCase())
 
 // // For LowerCase
 
-var LowerType = "IF YOU DON'T KNOW ABOUT ANY RESERVED KEYWORD YOU DON'T BE A PROGRAMMER.";
+var LowerType = "IF YOU DON'T KNOW ABOUT ANY RESERVED KEYWORD YOU DON'T BE A PROGRAMMER. IF YOU KNOW ABOUT THE RESERVED KEY YOU WILL BE A JS PROGRAMMER";
 console.log(LowerType.toLowerCase())
