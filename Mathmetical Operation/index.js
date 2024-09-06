af106389cad2e528adc5db7f1cc44b4008998868
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-06 18:05:55 +0600

    Add New Code

---
+++ Mathmetical Operation/index.js
@@ -0,0 +1,39 @@
+// For Sumation
+
+var num1 = 10;
+var num2 = 20;
+
+var total1 = num1 + num2;
+console.log(total1)
+
+// For Subtract
+
+var num3 = 30;
+var num4 = 40;
+
+var total2 = num4 - num3;
+console.log(total2)
+
+// For Multipication
+
+var num5 = 10;
+var num6 = 30;
+
+var total3 = num5 * num6;
+console.log(total3)
+
+// For Division
+
+var num7 = 300;
+var num8 = 10;
+
+var total4 = num7 / num8;
+console.log(total4)
+
+// For Reminder
+
+var num9 = 40;
+var num10 = 30;
+
+var total5 = num9 % num10;
+console.log(total5)
---
+++ Predefine Function/index.js
@@ -0,0 +1,32 @@
+// For Convert String to a Number(parseFloat)
+
+var  num1 = "10.05";
+var num2 = 15;
+
+num1 = parseFloat(num1); //In there we convert String into Number for Float Number
+var total1 = num1 + num2;
+
+console.log(total1)
+
+// For Convert String to a Number(parseInt)
+
+var num3 = 85;
+var num4 = 75.55;
+
+num4 = parseInt(num4); //In there we convert String into Number for Int Number
+var total2 = num3 + num4;
+
+console.log(total2)
+
+//For Convert Number to a String
+
+var num5 = 90;
+var num6 = 30;
+
+num6 = "" + num6;
+var total3 = num6 + num5; //In there we convert Number into String
+
+console.log(total3)
+
+
+
