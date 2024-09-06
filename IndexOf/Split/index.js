78c494e89c42541421ad0d3120215cf52978d88f
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-06 10:53:42 +0600

    Add IndexOf and Split Codes

---
+++ IndexOf/Split/index.js
@@ -0,0 +1,9 @@
+// For Split
+
+var myFd = "Nabil, Emon, Nurul, Mursalin,Zahin, Jubayer, Rony, Arafat";
+console.log(myFd.split("Rony"))
+
+// For IndexOf
+
+var myFd = "Nabil Nurul Zahin Jubayer";
+console.log(myFd.indexOf("Nurul"))
--- UpperCase/LowerCase/index.js
+++ UpperCase/LowerCase/index.js
@@ -1,9 +1,9 @@
-// For UpperCase
+// // For UpperCase
 
 var UpperType = "at first you will know about varaiable declaration.";
 console.log(UpperType.toUpperCase())
 
-// For LowerCase
+// // For LowerCase
 
 var LowerType = "IF YOU DON'T KNOW ABOUT ANY RESERVED KEYWORD YOU DON'T BE A PROGRAMMER.";
 console.log(LowerType.toLowerCase())
