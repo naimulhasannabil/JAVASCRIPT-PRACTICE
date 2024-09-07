f93f1eb80e28e8c9e9ff553e305e2a3256b43ef7
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-07 20:45:56 +0600

    This is a Practice Assignment

---
+++ Assignment/assignment.js
@@ -0,0 +1,64 @@
+// 1. What is Variable?
+// Answer: Variable are Containers for Storing Data.
+
+// 2. How to Write a Variable?
+// Answer: 
+var msgType = "Hi, My Name is Naimul Hasan Nabil.";
+console.log(msgType)
+
+// 3. What is String Type Variable?
+// Answer:
+var strType = "100";
+console.log(typeof strType)
+
+// 4. What is Number Type Variable?
+// Answer:
+var numType = 10;
+console.log(typeof numType)
+
+// 5. What is Boolean Type Variable?
+// Answer:
+var BoolType = true;
+console.log(typeof BoolType)
+
+// 6. How to use toUpperCase() & toLowerCase() ?
+// Answer for UpperCase:
+var someMsg1 = "This is my first Practice.";
+console.log(someMsg1.toUpperCase())
+
+// Answer for LowerCase:
+var someMsg2 = "I WILL BECAME A GOOD PROGRAMMER, IN SHA ALLAH."
+console.log(someMsg2.toLowerCase())
+
+// 7. How Many Operator in JavaScript ?
+// Answer: Operator used to perform different types of mathematical and logical computations. they are:
+//Addition(+), Subtraction(-), Multipication(*), Division(/), Modulus(%) etc.
+
+// 8. How to use Math.abs()?
+// Answer:
+var num1 = -10;
+abs = Math.abs(num1);
+console.log(abs)
+
+// 9. How to use Math.ceil()?
+// Answer:
+var num2 = 0.66;
+ceil = Math.ceil(num2);
+console.log(ceil)
+
+// 10. How to use Math.floor()?
+// Answer:
+var num3 = 0.99;
+floor = Math.floor(num3)
+console.log(floor)
+
+// 11. How to use Math.round()?
+// Answer:
+var num4 = 0.5;
+round = Math.round(num4)
+console.log(round)
+
+// 12. How to use Math.random()?
+// Answer: 
+var num5 = Math.random();
+console.log(num5) 
