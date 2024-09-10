6ec09d80c66e3ff26fde4fa0d66d47833b9a9033
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-10 20:09:23 +0600

    Add if-elseif-else Practice Code

--- if-else/index.js
+++ if-else/index.js
@@ -52,3 +52,19 @@ if(score >= 80){
    grade = "F";
 }
 console.log("Your Grade is: " + grade);
+
+//Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+
+var age = 61;
+var ticketPrice;
+
+if(age < 12){
+    ticketPrice = 5;
+} else if(age >= 12 && age < 18){
+    ticketPrice = 10;
+} else if(age >= 18 && age < 60){
+    ticketPrice = 20;
+} else {
+    ticketPrice = 15;
+}
+console.log("Ticket Price: $" + ticketPrice);
