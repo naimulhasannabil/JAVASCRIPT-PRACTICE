1b3343be27de9e3fe35e4ad5dc4eea2863a3896c
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-14 19:19:23 +0600

    Upload Array Code

---
+++ Array/index.js
@@ -0,0 +1,9 @@
+//Basics
+console.log("This is the main Array.")
+var fdName = ["a", "b", "c", "d", "e", "f"];
+console.log(fdName); //print the array
+
+console.log("After chanced"); 
+//Change Value
+fdName[5] = 'a'; //change the value
+console.log(fdName);
