3c37d38f5ffc82a652cece9d19e1110a3d2ab315
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-14 20:05:09 +0600

    Update the Code

--- Array/index.js
+++ Array/index.js
@@ -7,3 +7,26 @@ console.log("After chanced");
 //Change Value
 fdName[5] = 'a'; //change the value
 console.log(fdName);
+
+console.log("After adding new value");
+
+//Add Value
+
+fdName.push('g'); //add a new value at the end
+console.log(fdName);
+
+console.log("After removing a value");
+
+//Remove Value
+
+fdName.pop(); //remove the last value
+console.log(fdName);
+
+//using shift and unshift methods
+
+console.log("After adding new value using shift method");
+
+fdName.shift(); //remove the first value
+fdName.unshift('h'); //add a new value at the start
+console.log(fdName);
+
