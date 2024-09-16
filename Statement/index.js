e85a16d32089139c442ffd007271ee34f4a4c615
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-16 15:56:44 +0600

    Upload Statement Code

---
+++ Statement/index.js
@@ -0,0 +1,16 @@
+// Basic Break statement
+for(var i = 0; i < 10; i++) {
+    if(i == 5){
+        break; // Exit the loop when i equals 5
+    }
+    console.log(i);
+}
+
+// Continue statement
+
+for(var i = 0; i < 10; i++) {
+    if(i == 5){
+        continue; // Skip the current iteration when i equals 5
+    }
+    console.log(i);
+}
