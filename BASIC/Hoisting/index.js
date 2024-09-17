05491365a04e95b7f3129c89b19fd122e5da721a
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-17 17:19:28 +0600

    Update the Hoisting Code

--- BASIC/Hoisting/index.js
+++ BASIC/Hoisting/index.js
@@ -24,4 +24,39 @@ console.log(myConst); // ReferenceError
 
 const myConst = 5;
 
+// Function Hoisting with Template String
+
+console.log(myFunction2("John Doe")); // Hello, John Doe!
+
+function myFunction2(name) {
+    return `Hello, ${name}!`;
+}
+
+// Function Hoisting with Arrow Function
+
+console.log(myFunction3(5, 6)); // 30
+
+const myFunction3 = (p1, p2) => p1 * p2;
+
+// Function Hoisting with Rest Parameter and Arrow Function 
+
+console.log(myFunction4(...[5, 6])); // 30
+
+const myFunction4 = (...param) => param.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+// Function Hoisting with Async/Await and Arrow Function 
+
+async function myFunction5() {
+    const response = await fetch('https://api.example.com/data');
+    const data = await response.json();
+    console.log(data);
+}
+
+myFunction5();
+
+const myFunction5 = async () => {
+    const response = await fetch('https://api.example.com/data');
+    const data = await response.json();
+    console.log(data);
+};
 
