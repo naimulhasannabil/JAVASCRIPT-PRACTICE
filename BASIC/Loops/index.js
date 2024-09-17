1810841794bf4bf5e902ef17634afcc4fba3896d
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-17 17:23:35 +0600

    Update the Loops Code

--- BASIC/Loops/index.js
+++ BASIC/Loops/index.js
@@ -76,3 +76,40 @@ outer: for (var i = 0; i < 5; i++) {
     }
 }
 
+// Switch Statement
+
+var day = "Saturday";
+
+switch (day) {
+    case "Sunday":
+        console.log("Today is Sunday");
+        break;
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        break;
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+    case "Saturday":
+        console.log("Today is Saturday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+// Function
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greet("Nabil");
