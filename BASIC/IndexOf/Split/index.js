e1b1df4428471315a5fa217a8e5d8981df790554
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-17 17:21:53 +0600

    Update the Indexof/Split Code

--- BASIC/IndexOf/Split/index.js
+++ BASIC/IndexOf/Split/index.js
@@ -7,3 +7,34 @@ console.log(myFd.split("Rony"))
 
 var myFd = "Nabil Nurul Zahin Jubayer";
 console.log(myFd.indexOf("Nurul"))
+
+// For Slice
+
+var myFd = "Nabil Nurul Zahin Jubayer";
+console.log(myFd.slice(0, 5))
+
+// For Replace
+
+var myFd = "Nabil, Emon, Nurul, Mursalin, Zahin, Jubayer, Rony, Arafat";
+console.log(myFd.replace("Nabil", "Nurul"))
+
+// For Concat
+
+var myFd = "Nabil";
+var myName = "Nurul";
+console.log(myFd + " " + myName)
+
+// For Repeat
+
+var myFd = "Nabil ";
+console.log(myFd.repeat(3))
+
+// For ToLowerCase
+
+var myFd = "Nabil NURUL ZAHIN JUBAYER";
+console.log(myFd.toLowerCase())
+
+// For ToUpperCase
+
+var myFd = "Nabil NURUL ZAHIN JUBAYER";
+console.log(myFd.toUpperCase())
