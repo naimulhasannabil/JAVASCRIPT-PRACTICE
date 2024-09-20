e41b59f488f1474a086e098bee3b1d2da02b1923
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-20 22:04:24 +0600

    Upload the Practice Code

---
+++ Modern JS/practice.js
@@ -0,0 +1,11 @@
+// Basic Practices
+var num = 10;
+var num = 15;
+console.log(num); // Variables are changeable and can be modified
+
+let num1 = 10;
+num1 = 20; // let are changeable and can be modified but when modifying the code don't add let keywords
+console.log(num1);
+
+const num2 = 10;
+console.log(num2); // const are not changeable and can't be modified
