926edacf0134c8b041ca99b449df323b93d1c8a7
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-20 22:11:19 +0600

    Update the code

--- Modern JS/practice.js
+++ Modern JS/practice.js
@@ -9,3 +9,19 @@ console.log(num1);
 
 const num2 = 10;
 console.log(num2); // const are not changeable and can't be modified
+
+// Arithmetic Operations
+
+var num3 = 10;
+var num4 = 5;
+
+var sum = num3 + num4;
+console.log(sum); // Output: 15
+
+var diff = num3 - num4;
+
+console.log(diff); // Output: 5
+
+var product = num3 * num4;
+
+console.log(product); // Output: 50
