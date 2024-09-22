c6421fed3927383d732c0f095971beb35618fdd6
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-22 08:55:51 +0600

    Update the Statement Code

--- Modern JS/00_JS_Statements/index.js
+++ Modern JS/00_JS_Statements/index.js
@@ -1,4 +1,19 @@
+// Statement is instruction that we given the program.
+
 console.log("Hello world!");
 
 document.getElementById('title').innerHTML = 'Hi, this is my first modern js code!';
 
+let sum = 150 + 50;
+console.log(sum);
+
+if (sum > 100) {
+    console.log("Sum is greater than 100");
+} else {
+    console.log("Sum is less than 100");
+}
+
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+sayHello("Naimul Hasan Nabil");
