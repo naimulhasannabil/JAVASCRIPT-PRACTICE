b607de47ce92f42c948f838cfe7024de0f9bb7c4
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-22 16:15:45 +0600

    Upload JS Operator Precedence Code

---
+++ Modern JS/06_JS_Operator_Precedence/script.js
@@ -0,0 +1,15 @@
+// JavaScript Operator precedence
+// JavaScript Operator precedence page(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence)
+// ----- ()  {}  []    _>priority
+// / * - +      _>priority
+
+console.log(5 + 6 - 3 / 2 * 4);   //output : 5
+
+/* 
+5 + 6 - 3 / 2 * 4
+= 5 + 6 - 1.5 *4
+= 5 + 6 - 6
+= 5 - 0
+=5
+*/
+
