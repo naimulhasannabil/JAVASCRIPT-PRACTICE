0c89fc603863dbbff0f9b7070daff55768f3d108
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-22 09:13:45 +0600

    Upload JS Syntax Code

---
+++ Modern JS/01_JS_Syntax/script.js
@@ -0,0 +1,15 @@
+let firstName = 'Nabil';
+let age = 22;
+
+ firstname = "Naimul"             //=> Snake Case
+ FirstName = "Hasan"             //=> Pascal Case
+ firstName = "Nabil"            //=> Camel Case
+
+console.log(firstName); 
+
+console.log(age + 1);
+
+function funcName() {
+    console.log('This is a function');
+}
+funcName();
