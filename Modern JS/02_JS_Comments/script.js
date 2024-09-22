fff5b3f54fda66aad96485b08c6b7749564bfcf7
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-22 09:55:03 +0600

    Upload JS Comments Code

---
+++ Modern JS/02_JS_Comments/script.js
@@ -0,0 +1,20 @@
+// single line comment
+/*
+  Multi line comment
+*/
+
+let firstName = 'Nabil';
+let age = 22;
+
+ firstname = "Naimul"   // Changing firstName         
+ FirstName = "Hasan"            
+ firstName = "Nabil"            
+
+console.log(firstName); 
+
+console.log(age + 1);
+
+function funcName() {
+    console.log('This is a function');
+}
+funcName();
