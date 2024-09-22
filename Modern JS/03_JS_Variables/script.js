e152e7f98a905dc57248451d952a543efc8232c5
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-22 11:16:39 +0600

    Update JS Variables Code

--- Modern JS/03_JS_Variables/script.js
+++ Modern JS/03_JS_Variables/script.js
@@ -1,14 +1,19 @@
 // JavaScript Variable
 // JavaScript variable names are case-sensitive
+// !You can't add number first
+// ? _ , $ acceptable but,
+// ! - is not acceptable 
 let firstName;
 let FirstName;    // These Two is the right way to create a variable
 // let Last Name; // !warning this not the way to create a variable. variable names cannot contain spaces.
 // let function; // !warning this not the way to create a variable. Variables can't be created same as reserved keywords
+let fruits = 'Orange';
+console.log(`${fruits} is my fav, ${fruits} I like to eat`);
 
 /*
-*var Variable 
-*You can redeclare variables
-*You can reassign variables
+* var Variable 
+* You can redeclare variables
+* You can reassign variables
 * It is Global scope
 * var is hosted
 */
@@ -27,8 +32,51 @@ var country = "Bangladesh";     //Ouput: Undefined
 var countrys;
 console.log(countrys);         //Ouput: Undefined
 
+/*
+* let Variable
+! You can't redeclare variables
+* You can reassign variables
+? It is Block scope
+! let is not hoisted
+*/
 
+let b = 10;
+b = 11;
 
+console.log(b);
 
-let fruits = 'Orange';
-console.log(`${fruits} is my fav, ${fruits} I like to eat`);
+{
+    let c = 20;
+    console.log(c);
+}
+
+/*console.log(people);
+ let people = "people";*/  // give us reference error
+
+ /*
+* const Variable
+! You can't redeclare variables
+! You can't reassign variables
+? It is Block scope
+! const is not hoisted
+? You can change the const object value 
+*/
+
+/*  const c = 10;
+ const c = 20; */   // it will show a error
+
+ const d = 10;
+{
+   console.log(d);
+}
+
+/* console.log(d); */  // it will show a error
+
+const person = {
+    firstName: 'John',
+    age : 21,
+    city: 'New York'
+}
+person.country = 'United States';
+person.age = 23;   // the age value is changed.
+console.log(person);
