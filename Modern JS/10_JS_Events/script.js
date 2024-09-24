72cea6ccd81d0829a07a6ff3299e9d50c7ebf5b0
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-24 19:09:08 +0600

    Upload JS Events Code

---
+++ Modern JS/10_JS_Events/script.js
@@ -0,0 +1,11 @@
+// JS Events
+function subscribe() {
+  document.getElementById("text").innerHTML = 'Subscribed';
+  document.getElementById("btn").innerHTML = 'Subscribed';
+  document.getElementById("btn").style.backgroundColor = '#262626';
+}
+
+function like(){
+    document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+}
+ 
