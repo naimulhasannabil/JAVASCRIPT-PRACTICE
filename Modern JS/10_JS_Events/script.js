5945938235c9bd9447a64e95073d6a614e77b23b
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-24 20:06:05 +0600

    Upload JS Events

--- Modern JS/10_JS_Events/script.js
+++ Modern JS/10_JS_Events/script.js
@@ -1,11 +1,50 @@
 // JS Events
 function subscribe() {
-  document.getElementById("text").innerHTML = 'Subscribed';
-  document.getElementById("btn").innerHTML = 'Subscribed';
-  document.getElementById("btn").style.backgroundColor = '#262626';
+  document.getElementById("text").innerHTML = "Subscribed";
+  document.getElementById("btn").innerHTML = "Subscribed";
+  document.getElementById("btn").style.backgroundColor = "#262626";
 }
 
-function like(){
-    document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+// function like(){
+//     document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+// }
+
+function keyPress() {
+  document.getElementById("text").innerHTML = "Key Presed 😊";
 }
- 
+
+// function onLoad() {
+//     alert('Texting JavaScript Onload');
+//   document.getElementById("text").innerHTML = "Website Loaded Properly 😍";
+// }
+
+function windowResize() {
+  document.getElementById("textarea").style.height = "100px";
+  document.getElementById("form").style.fontFamily = "Time New Roman";
+}
+
+function onScroll() {
+  document.getElementById("text").innerHTML = "Scrolling 😊";
+}
+
+//   document.getElementById('btn').onclick = function () {
+//        document.getElementById('btn').innerHTML = 'Subscribed 😍';
+//   }
+
+//modern Practices
+
+document.getElementById("ul").addEventListener(
+  "click",
+  function (e) {
+    console.log('Ul Clicked');
+  },
+  true
+);
+
+document.getElementById("li").addEventListener(
+    "click",
+    function (e) {
+      console.log('LI Clicked');
+    },
+    true
+  );
