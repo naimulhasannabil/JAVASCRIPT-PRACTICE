d5afc3d6796cfdd9299a9d392dae4cdb5bd3e9ee
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-24 22:02:11 +0600

    Upload JS Math Code

---
+++ Modern JS/11_JS_Math/script.js
@@ -0,0 +1,32 @@
+// JavaScript Math
+
+let num = 30.24545982375;
+let num2 = 2;
+// If you need Round Number
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+// Square Value
+// console.log(Math.pow(3, num2));
+
+// Root Value
+// console.log(Math.sqrt(64));
+
+// Absolute Value
+// console.log(Math.abs(-30));
+
+// minimum value
+// console.log(Math.min(1,2,3,4,5,6));
+
+// maximum value
+// console.log(Math.max(1,2,3,4,5,6));
+
+// PI value
+// console.log(Math.PI);
+
+// cosine value
+// console.log(Math.sin(90 * Math.PI / 180));
+
+// Random value
+// console.log(Math.floor(Math.random().toFixed(2) * 10 + 1));
