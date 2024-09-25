31809032d3ae188ca0ec0a14c8bd8ecf43075977
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-25 19:25:10 +0600

    UUpdate the Code

--- Modern JS/13_JS_Date/script.js
+++ Modern JS/13_JS_Date/script.js
@@ -1,1 +1,14 @@
 //Javascript Date
+const myDate = new Date();
+
+console.log(myDate);   // run to console
+console.log(myDate.toString());  // Run in Terminal
+console.log(typeof myDate);  // type checking
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString()); 
+
+// When you need to modify the month fullName
+console.log(myDate.toLocaleString('default' ,{
+   weekday : 'long',
+})
+);
