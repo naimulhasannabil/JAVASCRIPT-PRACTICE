ea5735a701954d8800240fdb8a9750c324d55c9e
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-25 19:10:28 +0600

    Update

--- Modern JS/12_JS_Number/script.js
+++ Modern JS/12_JS_Number/script.js
@@ -3,11 +3,10 @@
 const x = 99999999999999;
 const y = 2.9;
 const z = 3.5;
-const m = (y + z).toFixed(2);    // string
+const m = (y + z).toFixed(2); // string
 // console.log(new Number(m));
 
 // console.log((y*10 + z*10)/10);
-
 
 const a = 10;
 const b = 20;
@@ -15,16 +14,14 @@ const c = 30;
 // if we add m in this console then we got all string numbers
 // console.log(m + a + b + c);
 
-// it will be different when we calculate multiplication, division and subtract 
-
-
+// it will be different when we calculate multiplication, division and subtract
 
 // When we calculate a exception in the code.
-const d = 'JavaScript';
+const d = "JavaScript";
 
 // console.log(c - d);     // output is NaN(Not a number)
 
-// infinity 
+// infinity
 // let num = 3;
 // let value ='';
 
@@ -36,7 +33,6 @@ const d = 'JavaScript';
 
 // Number Convert
 // console.log(c.toString(2));
-
 
 const n = 300;
 const r = new Number(300);
