08f68a65cf7be3a186074d09b8808022d5302c28
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-27 17:35:23 +0600

    update the Code

--- Modern JS/16_JS_Loops/script.js
+++ Modern JS/16_JS_Loops/script.js
@@ -7,3 +7,17 @@ car.push('Bugatti');
 for (let i = 0; i < car.length; i++) {
   console.log(car[i]);
 }
+
+// For In Loop (it work when you worked with the object)
+
+let person = {
+    name: 'Nabil',
+    age: 23,
+    city: 'Bangladesh',
+};
+
+for (let i in person){
+    // console.log(person[i]);
+    let capLetter = i.charAt(0).toUpperCase() + i.slice(1);  // When you need to Uppercase letter in your Object
+    console.log(capLetter + ': ' + person[i]); // You see all the items you inserted in the object
+}
