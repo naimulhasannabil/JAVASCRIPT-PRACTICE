1dc49e35626c8edf4456d24bf0af4ed0ea3bd741
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-27 17:05:57 +0600

    Update the Code

---
+++ Modern JS/15_JS_Statements/script.js
@@ -0,0 +1,11 @@
+// JavaScript Conditional Statements
+
+let age = 30;
+
+if (age > 18) {
+    console.log('You are ready to open a google account');
+} else if (age > 40) {
+    console.log('You are Old');
+} else {
+    console.log('You are not ready');
+}
