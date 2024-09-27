53f592be143b656b8d0cda601322bc099b2f964e
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-27 17:26:07 +0600

    Update the Code

---
+++ Modern JS/16_JS_Loops/script.js
@@ -0,0 +1,9 @@
+// JavaScript Loop
+
+let car = ['BMW', 'Volvo', 'Tesla', 'Lamborghini'];
+// ADD New items
+car.push('Bugatti');
+
+for (let i = 0; i < car.length; i++) {
+  console.log(car[i]);
+}
