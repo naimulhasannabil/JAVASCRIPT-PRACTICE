804ed07a03217532d64296ec2b8b7bb084d5ba90
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-27 17:38:14 +0600

    Update the Code

--- Modern JS/16_JS_Loops/script.js
+++ Modern JS/16_JS_Loops/script.js
@@ -1,23 +1,29 @@
 // JavaScript Loop
 
-let car = ['BMW', 'Volvo', 'Tesla', 'Lamborghini'];
+let car = ["BMW", "Volvo", "Tesla", "Lamborghini"];
 // ADD New items
-car.push('Bugatti');
+car.push("Bugatti");
 
-for (let i = 0; i < car.length; i++) {
-  console.log(car[i]);
-}
+// for (let i = 0; i < car.length; i++) {
+//   console.log(car[i]);
+// }
 
 // For In Loop (it work when you worked with the object)
 
 let person = {
-    name: 'Nabil',
-    age: 23,
-    city: 'Bangladesh',
+  name: "Nabil",
+  age: 23,
+  city: "Bangladesh",
 };
 
-for (let i in person){
-    // console.log(person[i]);
-    let capLetter = i.charAt(0).toUpperCase() + i.slice(1);  // When you need to Uppercase letter in your Object
-    console.log(capLetter + ': ' + person[i]); // You see all the items you inserted in the object
+for (let i in person) {
+  // console.log(person[i]);
+  let capLetter = i.charAt(0).toUpperCase() + i.slice(1); // When you need to Uppercase letter in your Object
+  console.log(capLetter + ": " + person[i]); // You see all the items you inserted in the object
 }
+
+// forEach Method
+car.forEach(function (i) {
+  console.log(i);
+});
+
