2d7c62db3fb272eaafb5dc4aee98328c11af2bad
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-29 19:32:03 +0600

    Update the Code

--- Modern JS/18_JS_Scope/script.js
+++ Modern JS/18_JS_Scope/script.js
@@ -3,16 +3,26 @@ let car = "Bentley";
 console.log(car);
 
 if (true) {
-    //Block Scope
+  //Block Scope
   console.log(car);
   let bike = "R15";
   console.log(bike);
+  if (true) {
+    let fish = "Goldfish";
+    console.log(fish);
+  }
 }
 
-function name() {
-    // Function Scope
+function firstName() {
+  // Function Scope
   let cycle = "Duronto";
   console.log(cycle);
   console.log(car);
+  function lastName() {
+    let fruits = "Mango";
+    console.log(fruits);
+    console.log(cycle);
+  }
+  lastName();
 }
-name();
+firstName();
