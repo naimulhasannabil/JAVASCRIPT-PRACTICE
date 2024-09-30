b82536826e554d8c020a1743c692a2a7c11bacd4
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 18:17:51 +0600

    Update the Code

--- Modern JS/21_JS_This/script.js
+++ Modern JS/21_JS_This/script.js
@@ -1,6 +1,6 @@
 // JavaScript This
 // Important thing is when we work this in node we get global Object and when we work with this in browser we get Window Object.
-console.log(this);
+// console.log(this);
 
 // this two var and function output will generate in window
 var firstName = "Nabil";
@@ -8,14 +8,25 @@ var firstName = "Nabil";
 function myFunction() {
   return this;
 }
-console.log(myFunction());
+// console.log(myFunction());
 
 const myFunc = () => {
-  console.log(this);
+  // console.log(this);
 };
 myFunc(); // You see that function and arrow function Both refer to Window
 
 const myBtn = document.querySelector('button');
 myBtn.addEventListener('click', function () {
-    console.log(this);   // Show html Button in the Console
-})
+    // console.log(this);   // Show html Button in the Console
+});
+
+// If you declare a method the method refer a object
+// If you declare a function the function refer to window / Global
+
+const student = {
+  fName : 'Nabil',
+  sInfo : function () {
+      return `${this.fName} loves to read`
+  }
+}
+console.log(student.sInfo());
