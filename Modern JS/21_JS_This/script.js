5aceaf44fb3ae86caba683c29d0bc7c5d754e462
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 18:31:18 +0600

    Update the Code

--- Modern JS/21_JS_This/script.js
+++ Modern JS/21_JS_This/script.js
@@ -15,18 +15,43 @@ const myFunc = () => {
 };
 myFunc(); // You see that function and arrow function Both refer to Window
 
-const myBtn = document.querySelector('button');
-myBtn.addEventListener('click', function () {
-    // console.log(this);   // Show html Button in the Console
+const myBtn = document.querySelector("button");
+myBtn.addEventListener("click", function () {
+  // console.log(this);   // Show html Button in the Console
 });
 
 // If you declare a method the method refer a object
 // If you declare a function the function refer to window / Global
 
 const student = {
-  fName : 'Nabil',
-  sInfo : function () {
-      return `${this.fName} loves to read`
-  }
-}
+  fName: "Nabil",
+  sInfo: function () {
+    return `${this.fName} loves to read`;
+  },
+};
 console.log(student.sInfo());
+
+// when you declare (this) in function it won't work
+
+function newPhone() {
+  let phone = "Apple";
+  console.log(this.phone);
+}
+newPhone(); // Output is Undefined
+
+// Bind
+
+const game = {
+  gName: "NfS",
+  gInfo: function () {
+    console.log(`Love to Play ${this.gName}`);
+  },
+};
+
+const aGame = {
+  gName: "Bike Game",
+};
+
+const bindGame = game.gInfo.bind(aGame);
+console.log(game.gInfo());
+console.log(bindGame());
