c7dff88911a8119e6032a3419c76472035976d93
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 08:03:22 +0600

    Update the Code

--- Modern JS/19_JS_Hoisting/script.js
+++ Modern JS/19_JS_Hoisting/script.js
@@ -1,1 +1,19 @@
 // JavaScript Hoisting
+
+// person = 'Naimul Hasan Nabil';
+// console.log(person);
+
+// Variable Hoisting
+
+var x;             // Declare
+x = 10;            //Initialized
+console.log(x);    //usage
+
+let y = 20;
+console.log(y);
+
+// Now let's see where actually hoisting work
+
+z = 30;            // Initialized Before Declare
+console.log(z);    // You will See there is no Error in the Console
+var z;            // Declare After Usage
