0e64fe8e7fbe22c1f4a6ae56cbafa027b5e53c52
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 19:04:43 +0600

    Update The code

--- Modern JS/22_JS_Memoization/script.js
+++ Modern JS/22_JS_Memoization/script.js
@@ -1,1 +1,37 @@
 // JavaScript Memoization
+
+function square(num) {
+  return num * num;
+}
+
+/*
+ * Name : memoSqr()
+ * This function creates a closure to Implement Memoization(caching results)
+ * It initialized an empty objects cache to store previously calculated square
+ * It Returns an inner function OptimizedSrq()
+ */
+
+function memoSqr() {
+  let cache = {};
+
+  return function OptimizedSrq(num) {
+      if (num in cache) {
+          console.log('Returning from Cache');
+          console.log(cache);    // to check Cache
+             return cache[num];
+      } else {
+        const result = square(num);
+        cache[num] = result;
+        console.log('Computing Square');
+       return result;
+      }
+  };
+}
+
+// Usage
+const memoSquare = memoSqr();
+
+console.log(memoSquare(4));
+console.log(memoSquare(4));
+console.log(memoSquare(5));
+console.log(memoSquare(5));
