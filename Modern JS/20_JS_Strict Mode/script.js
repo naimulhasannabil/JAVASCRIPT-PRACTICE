0b4aee5879e6334dcd321eec7e863d885a30aa63
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 09:06:20 +0600

    Update the Code

--- Modern JS/20_JS_Strict Mode/script.js
+++ Modern JS/20_JS_Strict Mode/script.js
@@ -4,15 +4,15 @@
 // x = 5;
 // console.log(x);     // it will show me a output until i use strict.
 
-// This Function will show me a output          
-name(4, 5);       
+// This Function will show me a output
+name(4, 5);
 function name(p1, p2) {
-    let y = 5;
-    console.log(p1, p2);
+  let y = 5;
+  console.log(p1, p2);
 }
 
 // This Function will show me a error when i use strict .
-// fullName(4, 5);       
+// fullName(4, 5);
 // function fullName(p1, p1) {
 //     let y = 5;
 //     console.log(p1, p1);
@@ -23,3 +23,5 @@ function name(p1, p2) {
 // let package = 'Some Text";
 // let public = 'Some Text";
 // let private = 'Some Text";
+
+// One more thing that when we used (use strict)at the top of the code than the whole code will useable in strict mode. when we use it in individual things just the individual things are useable for (use Strict) Mode
