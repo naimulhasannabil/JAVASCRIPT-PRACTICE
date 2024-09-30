27fe07264d4de175e69f9136fdded83ca1656559
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 09:49:12 +0600

    Update Html and CSS Code

--- Modern JS/21_JS_This/script.js
+++ Modern JS/21_JS_This/script.js
@@ -2,12 +2,20 @@
 // Important thing is when we work this in node we get global Object and when we work with this in browser we get Window Object.
 console.log(this);
 
-
 // this two var and function output will generate in window
-var firstName = 'Nabil';
+var firstName = "Nabil";
 
 function myFunction() {
-    return this;
+  return this;
 }
 console.log(myFunction());
 
+const myFunc = () => {
+  console.log(this);
+};
+myFunc(); // You see that function and arrow function Both refer to Window
+
+const myBtn = document.querySelector('button');
+myBtn.addEventListener('click', function () {
+    
+})
