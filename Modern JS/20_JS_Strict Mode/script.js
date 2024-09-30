e7933360dca71d2ecaeef92e8a1ffbfcae3965f2
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-30 08:49:35 +0600

    Update the Code

---
+++ Modern JS/20_JS_Strict Mode/script.js
@@ -0,0 +1,12 @@
+// JavaScript Strict Mode
+// 'use strict';
+
+// x = 5;
+// console.log(x);     // it will show me a output until i use strict.
+
+// This Function will show me a output          
+name(4, 5);       
+function name(p1, p2) {
+    let y = 5;
+    console.log(p1, p2);
+}
