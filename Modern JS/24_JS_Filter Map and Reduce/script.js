904a9e132f7b2643225b089b0e80711056e53f79
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-02 19:16:57 +0600

    Update The Code

--- Modern JS/24_JS_Filter Map and Reduce/script.js
+++ Modern JS/24_JS_Filter Map and Reduce/script.js
@@ -6,14 +6,24 @@ const fruits = ["Mango", "Banana", "Orange", "Apple"];
 
 // Character-wise Output
 const fruitCapB = fruits.filter((fruit) => fruit.toLowerCase().startsWith("b"));
-console.log(fruitCapB);
+// console.log(fruitCapB);
 
 // Lengthwise Output.
 const fruitsLen5 = fruits.filter((fruit) => fruit.length === 6);
-console.log(fruitsLen5);
+// console.log(fruitsLen5);
 
 // Includes Output
 const fruitsStartG = fruits.filter((fruit) =>
   fruit.toLowerCase().includes("g")
 );
-console.log(fruitsStartG);
+// console.log(fruitsStartG);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const res5 = numbers.filter((num) => num > 3);
+console.log(res5);
+
+// When you use curly braces in arrow function you must use return like this.
+const res6 = numbers.filter((num) => {
+  return num > 2;
+});
+console.log(res6);
