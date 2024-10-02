6d329b9ae947d9e052e8a47280efbc6411f17d89
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-02 17:34:22 +0600

    Update the Code

--- Modern JS/23_JS_Higher Order Array/script.js
+++ Modern JS/23_JS_Higher Order Array/script.js
@@ -1,17 +1,31 @@
 // JavaScript Higher Order Array Loop
 
-const fruits = ['Mango', 'Banana', 'Orange', 'Jack-Fruit'];
-for(let f = 0; f < fruits.length; f++){
-   console.log(fruits[f]);
+const fruits = ["Mango", "Banana", "Orange", "Jack-Fruit"];
+for (let f = 0; f < fruits.length; f++) {
+  //    console.log(fruits[f]);
 }
 
-// for of 
+// for of
 for (const f of fruits) {
-    console.log(f);     // It is easier than For loop.
+  // console.log(f);     // It is easier than For loop.
 }
 
 // Break The characters.
-const myName = 'Naimul Hasan Nabil';
+const myName = "Naimul Hasan Nabil";
 for (const name of myName) {
-    console.log(`Each char is ${name}`);
+  // console.log(`Each char is ${name}`);
+}
+
+// Map
+const country = new Map([
+  ["Bn", "Bangladesh"],
+  ["In", "India"],
+  ["Pk", "Pakistan"],
+  ["Np", "Nepal"],
+]);
+// Add New items
+country.set("Bhu", "Bhutan");
+// country.set('Bn', 'Bangladesh');   // Don't Add same type Data.
+for (const key of country) {
+  console.log(key);
 }
