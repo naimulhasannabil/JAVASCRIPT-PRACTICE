60a449abe5b548d5461c73096cc70c20d6c72981
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-02 17:19:08 +0600

    Update the Code

--- Modern JS/23_JS_Higher Order Array/script.js
+++ Modern JS/23_JS_Higher Order Array/script.js
@@ -4,3 +4,8 @@ const fruits = ['Mango', 'Banana', 'Orange', 'Jack-Fruit'];
 for(let f = 0; f < fruits.length; f++){
    console.log(fruits[f]);
 }
+
+// for of 
+for (const f of fruits) {
+    console.log(f);     // It is easier than For loop.
+}
