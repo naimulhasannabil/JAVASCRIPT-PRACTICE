de199c15c83085f3c60ffdf62f83efdd4cadb088
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-03 15:40:33 +0600

    Update The Code

--- Modern JS/25_JS_Call and Apply Method/script.js
+++ Modern JS/25_JS_Call and Apply Method/script.js
@@ -27,3 +27,15 @@ book.call(shohag, "A1", "Mahmudul Haque Emon");
 book.call(shohag, "A2", "Zahin Afser");
 // book("A1", "Mahmudul Haque Emon");
 console.log(shohag);
+
+// Add New Bus
+const hanif = {
+  bus: "Hanif",
+  bCode: "HN",
+  ticket: [],
+};
+book.call(hanif, "B2", "Mursalin Parbez");
+
+// Apply Method
+const ticketDate = ["B2", "Mursalin Parbez"];
+book.apply(greenLine, ticketDate);
--- Modern JS/practice.js
+++ Modern JS/practice.js
@@ -50,10 +50,10 @@ console.log(num6); // Output: 4
 
 // object
 var person = {
-    name : "Naimul Hasan Nabi",
-    age :  23,
-    city : "Dhaka"
-}
+  name: "Naimul Hasan Nabi",
+  age: 23,
+  city: "Dhaka",
+};
 
 console.log(person.name); // Output: Naimul Hasan Nabi
 
@@ -74,7 +74,7 @@ console.log(names[1]); // Output: Naimul
 // Function
 
 function greet() {
-    console.log("Hello World!");
+  console.log("Hello World!");
 }
 
 greet(); // Output: Hello World!
@@ -82,13 +82,13 @@ greet(); // Output: Hello World!
 // Function with Parameter
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 console.log(add(5, 6)); // Output: 11
 
 function sub(a, b) {
-    return a - b;
+  return a - b;
 }
 
 console.log(sub(10, 5)); // Output: 5
@@ -96,14 +96,22 @@ console.log(sub(10, 5)); // Output: 5
 let numS = `5 * "2"`;
 console.log(numS);
 
-
-function bigLetter (a) {
-    console.log(a.toUpperCase());
+function bigLetter(a) {
+  console.log(a.toUpperCase());
 }
-bigLetter('the quick brown fox');
+bigLetter("the quick brown fox");
 
 // Hoisting
 
 // x = 10;
 // console.log(x);
 // let x;    // Hoisting
+
+// Bus Company Practice
+
+
+const greenLine = {
+  bus: "Green-Line",
+  bCode: "GL",
+  ticket: [],
+};
