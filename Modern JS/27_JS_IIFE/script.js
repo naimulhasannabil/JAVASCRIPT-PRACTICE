a15ba312b46a4aa9ff603926eead562aeee9a964
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-16 20:25:43 +0600

    Update JS IIFE Code

--- Modern JS/27_JS_IIFE/script.js
+++ Modern JS/27_JS_IIFE/script.js
@@ -25,3 +25,22 @@ aFunction(7, 5);
   h3.style.backgroundColor = "red";
   h4.style.backgroundColor = "Green";
 })();
+
+// Stored in Variable.
+const myModule = (function () {
+  let privateVar = "This is a Private Variable";
+  function privateFunc() {
+    console.log("This is a Private Function");
+  }
+  return {
+    publicVar: "This is a Public Variable",
+    pubMethod: function () {
+      return "This is a Public Method";
+    },
+  };
+})();
+
+//console.log(myModule.privateVar);    //Unaccessible
+//console.log(myModule.privateFunc);     // Unaccessible
+console.log(myModule.publicVar); //Accessible
+console.log(myModule.pubMethod());
