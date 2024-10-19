f3664796deddfb03069a27d62c6201c28c8a83c7
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-19 10:52:48 +0600

    Update JS DOM Code

--- Modern JS/28_JS_DOM/script.js
+++ Modern JS/28_JS_DOM/script.js
@@ -1,7 +1,17 @@
 // JavaScript DOM
 document.querySelector("h1").innerText = "Subscribe";
 
-const box = document.querySelector("#box");
+
+const box = document.querySelector(".box");
 box.style.backgroundColor = "red";
 box.style.width = "100px";
 box.style.height = "100px";
+
+// //This Box is selected by Class.
+// const box1 = document.querySelector(".box1");
+// box.style.backgroundColor = "green";
+// box.style.width = "100px";
+// box.style.height = "100px";
+
+//Another way
+document.getElementById('box').style.backgroundColor = 'blue';
