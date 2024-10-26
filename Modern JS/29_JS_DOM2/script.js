68563f88b8e392f4bfafac6104829a033e0e6a29
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-26 11:27:25 +0600

    Update JS DOM2 Code

--- Modern JS/29_JS_DOM2/script.js
+++ Modern JS/29_JS_DOM2/script.js
@@ -1,4 +1,11 @@
 //JavaScript DOM
+// Add text inside tags
 let headTitle = document.querySelector('h1');
 // headTitle.innerText = 'Hello Naimul Hasan Nabil';      // Direct Change HTML Value
 headTitle.innerText = headTitle.innerText + " Naimul Hasan Nabil";     // Concat HTML Value
+
+// Change Text
+let boxes = document.querySelectorAll('.box');
+boxes[0].innerText = 'Box 1';
+boxes[1].innerText = 'Box 2';
+boxes[2].innerText = 'Box 3';
