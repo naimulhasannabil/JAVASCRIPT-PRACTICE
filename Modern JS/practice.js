7879bb1d0298d5933aae9a9679d76125c9c13bee
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-26 10:48:50 +0600

    Update JS Practice Code

--- Modern JS/practice.js
+++ Modern JS/practice.js
@@ -1,105 +1,105 @@
-// Basic Practices
-var num = 10;
-var num = 15;
-console.log(num); // Variables are changeable and can be modified
+// // Basic Practices
+// var num = 10;
+// var num = 15;
+// console.log(num); // Variables are changeable and can be modified
 
-let num1 = 10;
-num1 = 20; // let are changeable and can be modified but when modifying the code don't add let keywords
-console.log(num1);
+// let num1 = 10;
+// num1 = 20; // let are changeable and can be modified but when modifying the code don't add let keywords
+// console.log(num1);
 
-const num2 = 10;
-console.log(num2); // const are not changeable and can't be modified
+// const num2 = 10;
+// console.log(num2); // const are not changeable and can't be modified
 
-// Arithmetic Operations
+// // Arithmetic Operations
 
-var num3 = 10;
-var num4 = 5;
+// var num3 = 10;
+// var num4 = 5;
 
-var sum = num3 + num4;
-console.log(sum); // Output: 15
+// var sum = num3 + num4;
+// console.log(sum); // Output: 15
 
-var diff = num3 - num4;
+// var diff = num3 - num4;
 
-console.log(diff); // Output: 5
+// console.log(diff); // Output: 5
 
-var product = num3 * num4;
+// var product = num3 * num4;
 
-console.log(product); // Output: 50
+// console.log(product); // Output: 50
 
-var quotient = num3 / num4;
+// var quotient = num3 / num4;
 
-console.log(quotient); // Output: 2
+// console.log(quotient); // Output: 2
 
-var remainder = num3 % num4;
+// var remainder = num3 % num4;
 
-console.log(remainder); // Output: 0
+// console.log(remainder); // Output: 0
 
-// Increment and Decrement Operations
+// // Increment and Decrement Operations
 
-var num5 = 5;
+// var num5 = 5;
 
-num5++;
+// num5++;
 
-console.log(num5); // Output: 6
+// console.log(num5); // Output: 6
 
-var num6 = 5;
+// var num6 = 5;
 
-num6--;
+// num6--;
 
-console.log(num6); // Output: 4
+// console.log(num6); // Output: 4
 
-// object
-var person = {
-  name: "Naimul Hasan Nabi",
-  age: 23,
-  city: "Dhaka",
-};
+// // object
+// var person = {
+//   name: "Naimul Hasan Nabi",
+//   age: 23,
+//   city: "Dhaka",
+// };
 
-console.log(person.name); // Output: Naimul Hasan Nabi
+// console.log(person.name); // Output: Naimul Hasan Nabi
 
-person.age = 24;
+// person.age = 24;
 
-console.log(person.age); // Output: 24
+// console.log(person.age); // Output: 24
 
-// Array
+// // Array
 
-var names = ["Nabil", "Nurul", "Mahmudul"];
+// var names = ["Nabil", "Nurul", "Mahmudul"];
 
-console.log(names[0]); // Output: Nabil
+// console.log(names[0]); // Output: Nabil
 
-names[1] = "Naimul";
+// names[1] = "Naimul";
 
-console.log(names[1]); // Output: Naimul
+// console.log(names[1]); // Output: Naimul
 
-// Function
+// // Function
 
-function greet() {
-  console.log("Hello World!");
-}
+// function greet() {
+//   console.log("Hello World!");
+// }
 
-greet(); // Output: Hello World!
+// greet(); // Output: Hello World!
 
-// Function with Parameter
+// // Function with Parameter
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-console.log(add(5, 6)); // Output: 11
+// console.log(add(5, 6)); // Output: 11
 
-function sub(a, b) {
-  return a - b;
-}
+// function sub(a, b) {
+//   return a - b;
+// }
 
-console.log(sub(10, 5)); // Output: 5
+// console.log(sub(10, 5)); // Output: 5
 
-let numS = `5 * "2"`;
-console.log(numS);
+// let numS = `5 * "2"`;
+// console.log(numS);
 
-function bigLetter(a) {
-  console.log(a.toUpperCase());
-}
-bigLetter("the quick brown fox");
+// function bigLetter(a) {
+//   console.log(a.toUpperCase());
+// }
+// bigLetter("the quick brown fox");
 
 // Hoisting
 
@@ -110,8 +110,8 @@ bigLetter("the quick brown fox");
 // Bus Company Practice
 
 
-const greenLine = {
-  bus: "Green-Line",
-  bCode: "GL",
-  ticket: [],
-};
+// const greenLine = {
+//   bus: "Green-Line",
+//   bCode: "GL",
+//   ticket: [],
+// };
