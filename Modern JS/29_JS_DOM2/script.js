58a77b9d4594fac4da774cb3c1725114045fc726
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-10-26 11:55:19 +0600

    Update JS DOM2 Code

--- Modern JS/29_JS_DOM2/script.js
+++ Modern JS/29_JS_DOM2/script.js
@@ -5,15 +5,25 @@ let headTitle = document.querySelector('h1');
 headTitle.innerText = headTitle.innerText + " Naimul Hasan Nabil";     // Concat HTML Value
 
 // Change Text
-let boxes = document.querySelectorAll('.box');
-boxes[0].innerText = 'Box 1';
-boxes[1].innerText = 'Box 2';
-boxes[2].innerText = 'Box 3';
+// let boxes = document.querySelectorAll('.box');
+// boxes[0].innerText = 'Box 1';
+// boxes[1].innerText = 'Box 2';
+// boxes[2].innerText = 'Box 3';
 
 
 //using loop to change all Box value
-let newNum = 1;
-for(box of boxes){
-    box.innerText = `Box No ${newNum}`;
-    newNum++;
-}
+// let newNum = 1;
+// for(box of boxes){
+//     box.innerText = `Box No ${newNum}`;
+//     newNum++;
+// }
+
+// Add Element
+let learnBtn = document.createElement('button');
+learnBtn.innerText = 'Learn More';
+
+let heroArea = document.querySelector('.hero');
+heroArea.append(learnBtn);   //Last
+// heroArea.prepend(learnBtn);  //Fast
+// heroArea.before(learnBtn);   // Out of hero area before
+// heroArea.after(learnBtn);     // Out of hero area after
