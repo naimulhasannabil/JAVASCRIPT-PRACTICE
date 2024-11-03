7df0141c6b9188957d9b7d88fd80c8ad8ee50f22
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-03 21:50:56 +0600

    Update JS DOM Project Code

--- Modern JS/30_JS_DOM_Project/script.js
+++ Modern JS/30_JS_DOM_Project/script.js
@@ -1,8 +1,23 @@
-const body = document.getElementById('body');
-const button = document.querySelectorAll('li');
+const body = document.getElementById("body");
+const button = document.querySelectorAll("li");
 
-
-const redColor = document.querySelector('.red');
-redColor.addEventListener('click', function() {
-    body.style.backgroundColor = '#ff7675';
-})
+// red
+const redColor = document.querySelector(".red");
+redColor.addEventListener("click", function () {
+  body.style.backgroundColor = "#ff7675";
+});
+//green
+const greenColor = document.querySelector(".green");
+greenColor.addEventListener("click", function () {
+  body.style.backgroundColor = "#55efc4";
+});
+//blue
+const blueColor = document.querySelector(".blue");
+blueColor.addEventListener("click", function () {
+  body.style.backgroundColor = "#74b9ff";
+});
+//pink
+const pinkColor = document.querySelector(".pink");
+pinkColor.addEventListener("click", function () {
+  body.style.backgroundColor = "#fd79a8";
+});
