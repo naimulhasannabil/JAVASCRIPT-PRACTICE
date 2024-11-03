d7ec93439606105c3413218446d4d1077c3723f1
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-03 21:58:03 +0600

    Update JS DOM PROJECT CODE

--- Modern JS/30_JS_DOM_Project/script.js
+++ Modern JS/30_JS_DOM_Project/script.js
@@ -1,23 +1,34 @@
 const body = document.getElementById("body");
 const button = document.querySelectorAll("li");
 
+// First Option(Easy Code)
 // red
-const redColor = document.querySelector(".red");
-redColor.addEventListener("click", function () {
-  body.style.backgroundColor = "#ff7675";
-});
+// const redColor = document.querySelector(".red");
+// redColor.addEventListener("click", function () {
+//   body.style.backgroundColor = "#ff7675";
+// });
 //green
-const greenColor = document.querySelector(".green");
-greenColor.addEventListener("click", function () {
-  body.style.backgroundColor = "#55efc4";
-});
+// const greenColor = document.querySelector(".green");
+// greenColor.addEventListener("click", function () {
+//   body.style.backgroundColor = "#55efc4";
+// });
 //blue
-const blueColor = document.querySelector(".blue");
-blueColor.addEventListener("click", function () {
-  body.style.backgroundColor = "#74b9ff";
-});
+// const blueColor = document.querySelector(".blue");
+// blueColor.addEventListener("click", function () {
+//   body.style.backgroundColor = "#74b9ff";
+// });
 //pink
-const pinkColor = document.querySelector(".pink");
-pinkColor.addEventListener("click", function () {
-  body.style.backgroundColor = "#fd79a8";
-});
+// const pinkColor = document.querySelector(".pink");
+// pinkColor.addEventListener("click", function () {
+//   body.style.backgroundColor = "#fd79a8";
+// });
+//purple
+// const purpleColor = document.querySelector(".purple");
+// purpleColor.addEventListener("click", function () {
+//   body.style.backgroundColor = "#a29bfe";
+// });
+
+// Better Option
+button.forEach(function (value) {
+   
+})
