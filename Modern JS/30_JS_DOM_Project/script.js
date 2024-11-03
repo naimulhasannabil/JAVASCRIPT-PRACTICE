02fa4b3724e8dffba931b9e31561411d037a6466
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-03 21:45:59 +0600

    Update JS DOM PROJECT CODE

---
+++ Modern JS/30_JS_DOM_Project/script.js
@@ -0,0 +1,8 @@
+const body = document.getElementById('body');
+const button = document.querySelectorAll('li');
+
+
+const redColor = document.querySelector('.red');
+redColor.addEventListener('click', function() {
+    body.style.backgroundColor = '#ff7675';
+})
