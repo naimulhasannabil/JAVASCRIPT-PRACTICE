1f1124f65720cf3cea9331e6874c8798270a5fb7
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-03 22:12:53 +0600

    Update JS DOM Project Code

--- Modern JS/30_JS_DOM_Project/script.js
+++ Modern JS/30_JS_DOM_Project/script.js
@@ -29,9 +29,34 @@ const button = document.querySelectorAll("li");
 // });
 
 // Better Option
+
+// Default
+// button.forEach(function (value) {
+//    value.addEventListener('click', function() {
+//      let className = this.classList.value;
+//      body.style.backgroundColor = className;
+//    })
+// })
+
 button.forEach(function (value) {
-   value.addEventListener('click', function() {
-     let className = this.classList.value;
-     body.style.backgroundColor = className;
-   })
-})
+  value.addEventListener("click", function () {
+    let className = this.classList[0];
+    let color = "";
+    if (className === 'red') {
+      color = "#ff7675";
+    }
+    if (className === 'green') {
+      color = "#55efc4";
+    }
+    if (className === 'blue') {
+      color = "#74b9ff";
+    }
+    if (className === 'pink') {
+      color = "#fd79a8";
+    }
+    if (className === 'purple') {
+      color = "#a29bfe";
+    }
+    body.style.backgroundColor = color;
+  });
+});
