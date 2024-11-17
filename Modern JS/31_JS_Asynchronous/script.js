c81f87f11244e0cc4ba3e8c561db74f9998286d8
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-17 10:34:25 +0600

    Update JS Async Code

--- Modern JS/31_JS_Asynchronous/script.js
+++ Modern JS/31_JS_Asynchronous/script.js
@@ -11,3 +11,14 @@
 // }
 // console.log('End');
 // a();
+
+// Sync
+// const a = function () {
+//   console.log("Function Working");
+//   let aTime = new Date().getTime;
+//   while (aTime + 5000 >= new Date().getTime()) {}
+//   console.log("Completed");
+// };
+// console.log("First Line");
+// a();
+// console.log("Last Line");
